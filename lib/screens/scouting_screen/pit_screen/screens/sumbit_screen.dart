b97a3c106c66f7c1f619be7568fe95be27a71e70
Kyle3082 <bevobuilder@gmail.com>
2026-02-05 16:05:29 -0600

    Update sumbit_screen.dart

--- lib/screens/scouting_screen/pit_screen/screens/sumbit_screen.dart
+++ lib/screens/scouting_screen/pit_screen/screens/sumbit_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:cluck_scout/model/data/pit_data.dart';
+//import 'package:cluck_scout/model/data/pit_data.dart';
 import 'package:cluck_scout/providers/match_scouting_provider.dart';
 import 'package:cluck_scout/providers/pit_scouting_provider.dart';
 
